6b5a50a1a0966f48a209d7a194cb4ac9100e8cb0
ss842 <ss842@duke.edu>
2017-09-18 13:17:55 -0400

    3 unit tests are passing, 2 are not

--- datavalidation_code.py
+++ datavalidation_code.py
@@ -5,50 +5,50 @@ import numpy as np
 HR_data = [0, 0]  # initialize a matrix to store data
 
 #Open CSV
-filename = 'ecg_data.csv'
+#filename = 'ecg_data.csv'
 
 
 # DATA VALIDATION
 def columncheck(filename): #checks number of columns
     with open(filename) as HR:
         csv_HR = csv.reader(HR)
-    for row in csv_HR:
-        if len(row)!= 2:
-            raise TypeError('Data is not organized in 2 columns, please check and try again.')
+        for row in csv_HR:
+            if len(row)!= 2:
+                raise TypeError('Data is not organized in 2 columns, please check and try again.')
 
 def headercheck(filename):  # checks presence of headers
     with open(filename) as HR:
-         csv_HR = csv.reader(HR)
-    header_row = csv_HR.next() #verify that this accesses the very first row
-    if (type(header_row[0]) != str) or (type(header_row[1]) != str):
-               raise TypeError('Data headers are not present, please check and try again.')
+        csv_HR = csv.reader(HR)
+        header_row = next(csv_HR)
+        if (type(header_row[0]) != str) or (type(header_row[1]) != str):
+            raise TypeError('Data headers are not present, please check and try again.')
 
 def datatypecheck(filename):  # checks float/int
     with open(filename) as HR:
-         csv_HR = csv.reader(HR)
-    next(csv_HR)
-    for row in csv_HR:
-        for x in range(0,2):
-            if (type(row[x])= str):
-                raise TypeError('Data is not of correct type, please check and try again')
+        csv_HR = csv.reader(HR)
+        next(csv_HR)
+        for row in csv_HR:
+            for x in range(0,2):
+                if (type(row[x])== str):
+                    raise TypeError('Data is not of correct type, please check and try again')
 
 def datapracticality(filename):  # checks that the signal range will make sense
     with open(filename) as HR:
         csv_HR = csv.reader(HR)
-    next(csv_HR)
-    for row in csv_HR:
-            if row[2]>=100: #check mV range for typical ECG Data
-                raise TypeError('Data seems irregular, please check and try again')
+        next(csv_HR)
+        for row in csv_HR:
+                if row[2]>=100: #check mV range for typical ECG Data
+                    raise TypeError('Data seems irregular, please check and try again')
 
 #converting to numpy array
 def dataextraction(filename):
     with open(filename) as HR:
         csv_HR = csv.reader(HR)
-    next(csv_HR)
-    for row in csv_HR:
-        time = float(row[0])
-        signal = float(row[1])
-        HR_data=np.vstack([HR_data, [time, signal]])
+        next(csv_HR)
+        for row in csv_HR:
+            time = float(row[0])
+            signal = float(row[1])
+            HR_data=np.vstack([HR_data, [time, signal]])
 
 
-HR.close()
+#HR.close()
--- test_import.py
+++ test_import.py
@@ -1,5 +1,6 @@
 import pytest
-import filename.py
+
+import datavalidation_code
 
 import numpy
 import sys
@@ -15,34 +16,43 @@ Test 1 - test  test that you are parsing the read data correctly into numpy arra
 3. 
 """
 
+#test that the data is correctly in 2 columns
+def testcolumns():
+    with pytest.raises(TypeError):
+        datavalidation_code.columncheck('FaultyData_UnitTest.csv')
+
 #test that the headers are present
 def testheader():
     with pytest.raises(TypeError):
-        filename.funcname(FaultyData_UnitTest.csv)
+        datavalidation_code.headercheck('FaultyData_UnitTest.csv')
 
 #test that the data type after the first row is all float/int
 def testdatatype():
     with pytest.raises(TypeError):
-        filename.funcname(FaultyData_UnitTest.csv)
-
-#test that the data is correctly in 2 columns
-def testcolumns():
-    with pytest.raises(TypeError):
-        filename.funcname(FaultyData_UnitTest.csv)
+        datavalidation_code.datatypecheck('FaultyData_UnitTest.csv')
 
 #test that the data values are within a practical range
 def testvaluerange():
     with pytest.raises(TypeError):
-        filename.funcname(FaultyData_UnitTest.csv)
+        datavalidation_code.datapracticality('FaultyData_UnitTest.csv')
 
 # test that the read data was parsed correctly into numpy arrays
 def testnumpyarray():
     with pytest.raises(TypeError):
-        filename.funcname(FaultyData_UnitTest.csv)
+        datavalidation_code.dataextraction('FaultyData_UnitTest.csv')
 
 
 #def teststring():
     #with pytest.raises(TypeError):
      #   sumfxn2.summation_func('a', 'b')
+
+def main():
+    print("Blah")
+    with open('FaultyData_UnitTest.csv') as HR:
+        csv_HR = csv.reader(HR)
+
+    for row in csv_HR:
+        print (row);
+
 
 
