2140f5f9a2d46de4332eaedea762e2cf776dbd23
Jawad Hoballah <Jawad@Jawads-MacBook-Pro.local>
2017-09-21 16:38:55 -0400

    added additional unit tests to check for tachy / brachy not being present

--- test_avghr.py
+++ test_avghr.py
@@ -17,9 +17,14 @@ def test_avghr():
     assert(avghr.hr_averaging(time_array, mv_array, averaging_time), average_hr_val)
 
 
-def test_tachy():
+def test_tachy_present():
     assert(avghr.tachy(200),  False)
 
+def test_tachy_not_present():
+    assert(avghr.tachy(80),  False)
 
-def test_brachy():
+def test_brachy_present():
     assert(avghr.brachy(30), False)
+
+ def test_brachy_not_present():
+    assert (avghr.brachy(75), False)
