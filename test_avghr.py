18ec0de1c96f3eb0f54826b556ca6d244bb2cf2c
Jawad Hoballah <Jawad@Jawads-MacBook-Pro.local>
2017-09-21 15:29:47 -0400

    Updated unit tests and included importing of avghr script

--- test_avghr.py
+++ test_avghr.py
@@ -3,6 +3,8 @@ import numpy
 import pytest_cov
 import pytest_pep8
 import unittest
+import avghr
+
 
 """
 
@@ -12,12 +14,12 @@ unit tests for average HR code
 
 
 def test_avghr():
-    assert(avghr(time, mV, avg_time), avghr_val)
+    assert(avghr.hr_averaging(time_array, mv_array, averaging_time), average_hr_val)
 
 
 def test_tachy():
-    assert(tachy(200), False)
+    assert(avghr.tachy(200), False)
 
 
 def test_brachy():
-    assert(brachy(30), False)
+    assert(avghr.brachy(30), False)
