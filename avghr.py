e4d1a4fbf9563cc9eaa14b0517aa0e97b6d1165b
Jawad Hoballah <Jawad@Jawads-MacBook-Pro.local>
2017-09-24 20:06:47 -0400

    Added new unit tests for hr_averaging

--- avghr.py
+++ avghr.py
@@ -1,8 +1,26 @@
 import numpy as np
+import sys
+import HR_peakdetect
+
+min_to_sec = 60
+num_arg = 3
+
 
 def hr_averaging(time_array, mv_array, averaging_time):
 
-    averaging_time_sec = averaging_time * 60
+    try:
+        num_arg == sys.argv
+    except ValueError:
+        print("Please input the correct number of arguments")
+
+    try:
+        averaging_time = complex(averaging_time)
+        averaging_time = averaging_time.real
+    except ValueError:
+        print("Your averaging_time input is not a number, please input a number.")
+
+    averaging_time_sec = averaging_time * min_to_sec
+    HR_peakdetect()
     avg_index = (np.abs(time_array - averaging_time_sec)).argmin()
 
     mv_array_sliced = mv_array[:avg_index]
@@ -11,14 +29,29 @@ def hr_averaging(time_array, mv_array, averaging_time):
 
     return average_hr_val
 
-def tachy(average_hr_val, tachy_limit = 100):
+
+def tachy(average_hr_val, tachy_limit=100):
+
+    try:
+        tachy_limit = complex(tachy_limit)
+        tachy_limit = tachy_limit.real
+    except ValueError:
+        print("Your tachycardia threshold input is not a number, please input a number.")
+
     if average_hr_val > tachy_limit:
         print("Tachycardia was found!")
         return False
     else:
         return True
 
-def brachy(average_hr_val, brachy_limit = 60):
+
+def brachy(average_hr_val, brachy_limit=60):
+
+    try:
+        brachy_limit = complex(brachy_limit)
+        brachy_limit = brachy_limit.real
+    except ValueError:
+        print("Your brachycardia threshold input is not a number, please input a number.")
 
     if average_hr_val < brachy_limit:
         print("Brachycardia was found!")
--- test_avghr.py
+++ test_avghr.py
@@ -1,10 +1,13 @@
 import pytest
-import numpy
+import numpy as np
 import pytest_cov
 import pytest_pep8
 import unittest
 import avghr
-
+import sys
+import math
+import ecg_data.csv
+import datavalidation
 
 """
 
@@ -12,19 +15,31 @@ unit tests for average HR code
 
 """
 
-
 def test_avghr():
-    assert(avghr.hr_averaging(time_array, mv_array, averaging_time), average_hr_val)
+    assert(avghr.hr_averaging(ecg_data.csv), 30)
 
 
 def test_tachy_present():
     assert(avghr.tachy(200),  False)
 
+
 def test_tachy_not_present():
     assert(avghr.tachy(80),  True)
+
 
 def test_brachy_present():
     assert(avghr.brachy(30), False)
 
+
 def test_brachy_not_present():
     assert (avghr.brachy(65), True)
+
+
+
+def test_averagingtime():
+    with pytest.raises(ValueError):
+        avghr.hr_averaging(t,y,)
+
+def teststring():
+    with pytest.raises(TypeError):
+        avghr.hr_averaging(t,y, "word")
