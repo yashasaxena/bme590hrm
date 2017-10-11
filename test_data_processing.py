ae8eeb6c069261af9d4419143d70197af00b96e9
ss842 <ss842@duke.edu>
2017-10-11 11:45:33 -0400

    changed reference file from datavalidation_code to processing_data

--- test_data_processing.py
+++ test_data_processing.py
@@ -14,27 +14,28 @@ import sys
 def testcolumns():
     """ unit test to throw an error if the data is not consistently in 2 columns"""
     with pytest.raises(TypeError):
-        datavalidation_code.columncheck('FaultyData_UnitTest.csv')
+        x = hr.data()
+        processing_data.columncheck('FaultyData_UnitTest.csv')
 
 #test that the data type after the first row is all float/int
 def testdatatype():
     """ unit test to throw an error if any data is string type"""
     with pytest.raises(ValueError):
-        datavalidation_code.datatypecheck('FaultyData_UnitTest.csv')
+        processing_data.datatypecheck('FaultyData_UnitTest.csv')
 
 #test that the data values are within a practical range
 def testvaluerange():
     """ unit test to throw an error if the data is above 10mV"""
     with pytest.raises(ValueError):
-        datavalidation_code.datapracticality('FaultyData_UnitTest.csv')
+        processing_data.datapracticality('FaultyData_UnitTest.csv')
 
 def testdataisgood():
     """ unit test to make sure that all data passes
     columncheck():, datatypecheck():, and datapracticality():
     and that no error is thrown when data behaves as expected"""
-    assert datavalidation_code.columncheck("ecg_data.csv")==1
-    assert datavalidation_code.datatypecheck("ecg_data.csv")==1
-    assert datavalidation_code.datapracticality("ecg_data.csv")==1
+    assert processing_data.columncheck("ecg_data.csv")==1
+    assert processing_data.datatypecheck("ecg_data.csv")==1
+    assert processing_data.datapracticality("ecg_data.csv")==1
 
 #create a sine wave array to test peak finder, instant heart rate function
 # f = 1 hz, T = 1000 ms
