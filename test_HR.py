8c1d27c7f03bcbf40a8bb33789cc696e2531b90b
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-10-12 22:05:23 -0400

    readding these test files identical to base branch to resolve merge conflict

---
+++ test_HR.py
@@ -0,0 +1,34 @@
+import HR_allfuncs as hr
+import numpy as np
+import pytest
+
+
+#create a sine wave array to test peak finder, instant heart rate function
+# f = 1 hz, T = 1000 ms
+# time step will be 1 ms (0.001 s), t array goes from 0 to 10,000 ms (10 s)
+# expected peaks should be 10
+
+t = np.arange(0, 10, 0.001)
+signal = abs(np.sin(t*np.pi)**3)
+
+#combine t and sin_vals arrays
+array_test = np.column_stack((t, signal))
+array_test_time = hr.HR_peakdetect(array_test)
+
+# unit test peak detection
+def test_peakdetect():
+    """
+    .. function:: test_peakdetect():
+    Tests if the number of peaks for a defined sine wave is returned by peak detection function
+    """
+    assert len(hr.HR_peakdetect(array_test)) == 10
+
+def test_instHR():
+    """
+        .. function:: test_instHR():
+        Tests if instant heart rate calculated is equal to T * 60s/min
+    """
+    x = hr.Vitals()
+    x.hr_averaging('3/20', array_test_time)
+    x.inst_hr_val = int(round(x.inst_hr_val))
+    assert x.inst_hr_val == 60
