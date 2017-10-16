92810a866d6888860ed39a10d7b930a2fb648f4c
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-10-15 20:02:25 -0400

    modified run files code to accomodate test_data folder path change

--- test_hrm_runfiles.py
+++ test_hrm_runfiles.py
@@ -7,7 +7,7 @@ import pytest
 def test_fileimport():
     """ Tests if all files are imported into directory
     """
-    for x in glob.glob('*.csv'):
+    for x in glob.glob('/test_data/*.csv'):
         JSY_HR.main(x,10,80,100, 'Patient' + str(file_counter) + '_hrResults.txt')
         file_counter = file_counter + 1
 
