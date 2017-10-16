93113772f79642d8b2cce6bd30266f7196ace065
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-10-16 13:05:56 -0400

    Patient...highest level class

---
+++ Patient.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy import signal
+
+# import subclass files
+import Data
+import Processing
+import Vitals
+import Diagnosis
+
+class Patient:
+    def __init__(self, filename = 'ecg_data.csv', avg_time, tachy_limit=100,
+                 brachy_limit=60, return_file = 'HR_Specs.txt'):
+        self.data = Data(filename)
+        self.pd_processing = Processing(self.data.HR_array)
+        self.vitals = Vitals(self.pd_processing.t)
+        self.diagnosis = Diagnosis(self.vitals.avg_HR)
+
+    def create_patient_file(self):
+        hr_txt = open(return_file, "w")
+        hr_txt.write("Patient Name: Heart Patient Dude\n")
+        hr_txt.write("Patient file: " + file_name + "\n")
+        hr_txt.write("Estimated instant heart rate: {:.3f}\n".format(instant_hr))
+        hr_txt.write("Estimated average heart rate: {:.3f}\n".format(average_hr))
+        hr_txt.write("Sign of tachycardia: " + str(tachy_present) + "\n")
+        hr_txt.write("Sign of brachycardia: " + str(brachy_present) + "\n")
+        hr_txt.write("Please note that values were extracted from a fixed sample of data.\n")
+        hr_txt.write("Consult your physician for further information and clarification.\n")
+
+        hr_txt.close()
+
+        # read from the file to command line
+        f = open(return_file)
+        for line in iter(f):
+            print(line)
+        f.close()
+
