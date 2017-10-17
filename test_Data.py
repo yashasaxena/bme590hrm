795f60f5ad2ac06964e7fe51b3748868bae39aa8
ss842 <ss842@duke.edu>
2017-10-17 01:12:40 -0400

    trying with np.genfromtxt but not recognizing my attribute readfile

--- test_Data.py
+++ test_Data.py
@@ -1,5 +1,5 @@
 import pytest
-import Data as SC
+import Data1 as SC
 
 
 def test_columns():
@@ -13,14 +13,14 @@ def test_datatype():
     """ unit test to throw an error if any data is string type"""
     with pytest.raises(TypeError):
         x = SC.Data('FaultyData_UnitTest2.csv')
-        x.type_check('FaultyData_UnitTest2.csv')
+        x.type_check()
 
 
 def test_valuerange():
     """ unit test to throw an error if the data is above 10mV"""
     with pytest.raises(ValueError):
         x = SC.Data('FaultyData_UnitTest2.csv')
-        x.practicality_check('FaultyData_UnitTest2.csv')
+        x.practicality_check()
 
 
 def test_dataisgood():
@@ -29,5 +29,5 @@ def test_dataisgood():
     and that no error is thrown when data behaves as expected"""
     x = SC.Data('GoodData_UnitTest.csv')
     assert x.column_check('GoodData_UnitTest.csv') == 1
-    assert x.type_check('GoodData_UnitTest.csv') == 1
-    assert x.practicality_check('GoodData_UnitTest.csv') == 1
+    assert x.type_check() == 1
+    assert x.practicality_check() == 1
