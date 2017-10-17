bcb633be6f7270b83c27e9950777b9438ee1aa6f
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-10-17 03:07:18 -0400

    input peak detect func modifications to Processing

--- Processing.py
+++ Processing.py
@@ -1,34 +1,40 @@
 import numpy as np
-from scipy import signal
+
 
 class Processing:
-# pseudocode for processing subclass
+
    def __init__(self):
        self.t = []
 
-   def peakdetect(self, data_array):
+   def ecg_peakdetect(self, data_array):
     """ Returns a time array of times where peak was detected
 
     :param data_array: a 2-d array with time and voltage values
     :rtype: array
     """
-    # peak detection function based on variable threshold method
-    diff_filter = 0.125 * np.array([2, 1, -1, -2])
-    # differentiation process window, baseline correction
-    pre_processing = data_array[:, 1]
-    pre_processing = np.convolve(pre_processing, diff_filter, 'same')
-    # inverting data because negative peaks have less peak noise surrounding them
-    inverted_data = np.multiply(-1, pre_processing)
-    # heart rates to test, lower spectrum to higher spectrum
-    rates = np.array(range(40, 200)) / 60
-    t_step = data_array[1, 0] - data_array[0, 0]
-    fs = int(1 / t_step)  # sampling frequency that provides number of steps in 1 second
-    peakind = signal.find_peaks_cwt(inverted_data, fs / rates / 10)
+        # peak detection function based on variable threshold method
+        diff_filter = 0.125 * np.array([2, 1, -1, -2])
+        # differentiation process/bandpass filter window, baseline correction
+        pre_processing = data_array[:, 1]
+        pre_processing = np.convolve(pre_processing, diff_filter, 'same')
 
-    time_array = []
+        max_peak = np.max(pre_processing)
+        threshold = max_peak - 0.5 * max_peak
+        peakind = []
+        time_array = []
+        time_array_clean = []
 
-    for c in range(0, len(peakind)):
-        y = peakind[c]
-        time_array.append(data_array[y, 0])
+        for c in range(0, len(pre_processing)):
+            if pre_processing[c] >= threshold:
+                peakind.append(c)
+                time_array.append(data_array[c, 0])
 
-    self.t = time_array
+        time_differences = np.diff(time_array)
+
+        for d in range(0, len(time_differences)):
+            if time_differences[d] > 0.3:
+                time_array_clean.append(time_array[d])
+
+        time_array_clean.append(time_array[-1])
+
+        self.t = time_array_clean
