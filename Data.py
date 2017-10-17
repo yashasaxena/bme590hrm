d37d5753bc29d5fe0b7ef438e0314907abd2bfc1
ss842 <ss842@duke.edu>
2017-10-17 17:51:49 -0400

    unit tests are passing and successful interpolation of strings and blanks in data

--- Data.py
+++ Data.py
@@ -4,12 +4,13 @@ import numpy as np
 class Data:
 
     def __init__(self, filename='ecg_testdata.csv'):
-        self.filename_array = np.genfromtxt(filename, delimiter=',', skip_header=1)
-        self.HR_data = np.array([0, 0])
-        # self.HR_data = np.genfromtxt(filename, delimiter=',', skip_header=1)
+        self.filename_array = np.genfromtxt(filename, delimiter=',', skip_header=1, missing_values='', filling_values=0.0)
+        # self.HR_data = np.array([0, 0])
+        self.HR_data = np.genfromtxt(filename, delimiter=',', skip_header=1, missing_values='', filling_values=0.0)
         self.column_check_result = None
         self.value_type_result = None
         self.value_range_result = None
+        self.f = np.array([0, 0])
 
 #   , missing_values='',filling_values=0.0
 
@@ -26,20 +27,20 @@ class Data:
         else:
             self.column_check_result = True
 
-    def value_type(self):
-        """ Confirms there are no strings in the data
-        :param: csv file with HR data
-        :rtype: Error raised if data type is incorrect
-        """
-
-        # for row in range(self.filename_array.shape[1]):
-        # if self.filename_array[row, 1] == np.nan
-        if np.isnan(self.filename_array).any():
-            print("The values in your array are not all numbers.")
-            self.value_type_result = False
-            raise TypeError
-        else:
-            self.value_type_result = True
+    # def value_type(self):
+    #     """ Confirms there are no strings in the data
+    #     :param: csv file with HR data
+    #     :rtype: Error raised if data type is incorrect
+    #     """
+    #
+    #     # for row in range(self.filename_array.shape[1]):
+    #     # if self.filename_array[row, 1] == np.nan
+    #     if np.isnan(self.filename_array).any():
+    #         print("The values in your array are not all numbers.")
+    #         self.value_type_result = False
+    #         raise TypeError
+    #     else:
+    #         self.value_type_result = True
 
     def value_range(self):
         """ Confirms that the signal is within an
@@ -69,11 +70,11 @@ class Data:
             print(ex)
             return
 
-        try:
-            self.value_type()
-        except Exception as ex:
-            print(ex)
-            return
+        # try:
+        #     self.value_type()
+        # except Exception as ex:
+        #     print(ex)
+        #     return
 
         try:
             self.value_range()
@@ -81,18 +82,20 @@ class Data:
             print(ex)
             return
 
-        # self.HR_data = np.genfromtxt(filename, delimiter=',', skip_header=1, missing_values='', filling_values=0.0)
-        f = self.filename_array  # self.HR_data if we can, so that we don't need line 95
+        f = self.HR_data
         for x in range(1, len(f[:, 0])):
-            if f[x, 0] == 0.0:  # if we can't do the missing_values np.gefromtxt down here,
-                # we will have to change "== 0.0"
+            if f[x, 0] == 0.0:
                 f[x, 0] = (f[x + 1, 0]+f[x - 1, 0]) / 2
+        for x in range(1, len(f[:, 0])):
+            if f[x, 1] == 0.0:
+                f[x, 1] = (f[x + 1, 0]+f[x - 1, 0]) / 2
+        self.f = f
 
         # for row in f:
         #     time = float(row[0])
         #     signal = float(row[1])
         # self.HR_data = np.vstack([self.filename_array, [time, signal]])
-        self.HR_data = self.filename_array
+        # self.HR_data = self.filename_array
         # HR.close()
         # self.HR_data = np.delete(self.HR_data, 0, axis=0)
 
--- test_Data.py
+++ test_Data.py
@@ -8,10 +8,10 @@ def test_column_number():
         x.column_check()
 
 
-def test_value_type():
-    with pytest.raises(TypeError):
-        x = Alternative_Data.Data('FaultyData_UnitTest.csv')
-        x.value_type()
+# def test_value_type():
+    # with pytest.raises(TypeError):
+    #     x = Alternative_Data.Data('FaultyData_UnitTest.csv')
+    #     x.value_type()
 
 def test_value_range():
     with pytest.raises(ValueError):
@@ -21,8 +21,8 @@ def test_value_range():
 def test_data_is_good():
     good_test_data = Alternative_Data.Data('GoodData_UnitTest.csv')
     good_test_data.column_check()
-    good_test_data.value_type()
+    # good_test_data.value_type()
     good_test_data.value_range()
     assert good_test_data.column_check_result is True
-    assert good_test_data.value_type_result is True
+    # assert good_test_data.value_type_result is True
     assert good_test_data.value_range_result is True
