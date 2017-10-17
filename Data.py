826a6181f42df52f43848332b66e1b12e1728721
ss842 <ss842@duke.edu>
2017-10-17 11:33:06 -0400

    moved unit test from Data to test_Data

--- Data.py
+++ Data.py
@@ -10,7 +10,14 @@ class Data:
         self.value_type_result = None
         self.value_range_result = None
 
+#   , missing_values='',filling_values=0.0
+
     def column_check(self):
+        """ Confirms that data is arranged in 2 columns
+        :param: csv file with HR data
+        :rtype: Error raised if data structure is incorrect
+        """
+
         if self.filename_array.shape[1] != 2:
             print("Your file does not have 2 columns.")
             self.column_check_result = False
@@ -19,16 +26,27 @@ class Data:
             self.column_check_result = True
 
     def value_type(self):
-        #for row in range(self.filename_array.shape[1]):
-           # if self.filename_array[row, 1] == np.nan
-            if np.isnan(self.filename_array).any():
-                print("The values in your array are not all numbers.")
-                self.value_type_result = False
-                raise TypeError
-            else:
-                self.value_type_result = True
+        """ Confirms there are no strings in the data
+        :param: csv file with HR data
+        :rtype: Error raised if data type is incorrect
+        """
+
+        # for row in range(self.filename_array.shape[1]):
+        # if self.filename_array[row, 1] == np.nan
+        if np.isnan(self.filename_array).any():
+            print("The values in your array are not all numbers.")
+            self.value_type_result = False
+            raise TypeError
+        else:
+            self.value_type_result = True
 
     def value_range(self):
+        """ Confirms that the signal is within an
+        expected range (below 300mV)
+        :param: csv file with HR data
+        :rtype: Error raised if mV values exceed 10mV
+        """
+
         # rows, columns = self.filename_array.shape
         for row in range(len(self.filename_array)):
             if self.filename_array[row, 1] >= 300:
@@ -38,11 +56,42 @@ class Data:
             else:
                 self.value_range_result = True
 
-    def test_data_is_good(self):
-        good_test_data = Data('GoodData_UnitTest.csv')
-        good_test_data.column_check()
-        good_test_data.value_type()
-        good_test_data.value_range()
-        assert good_test_data.column_check_result is True
-        assert good_test_data.value_type_result is True
-        assert good_test_data.value_range_result is True
+    def extraction(self, filename):
+        """ Opens CSV file, converts all numbers to float type,
+        then creates an array to append rows
+        :param filename: csv file with HR data
+        :rtype: HR_data as the matrix for data analysis """
+
+        try:
+            self.column_check()
+        except Exception as ex:
+            print(ex)
+            return
+
+        try:
+            self.value_type()
+        except Exception as ex:
+            print(ex)
+            return
+
+        try:
+            self.value_range()
+        except Exception as ex:
+            print(ex)
+            return
+
+        self.HR_data = np.genfromtxt(filename, delimiter=',', skip_header=1, missing_values='', filling_values=0.0)
+        f = self.HR_data
+        for x in range(1, len(f[:, 0])):
+            if f[x, 0] == 0.0:
+                f[x, 0] = (f[x + 1, 0]+f[x - 1, 0]) / 2
+
+        # for row in f:
+        #     time = float(row[0])
+        #     signal = float(row[1])
+        # self.HR_data = np.vstack([self.filename_array, [time, signal]])
+        # self.HR_data = self.filename_array
+        # HR.close()
+        # self.HR_data = np.delete(self.HR_data, 0, axis=0)
+
+        return self.HR_data
--- test_Data.py
+++ test_Data.py
@@ -17,3 +17,12 @@ def test_value_range():
     with pytest.raises(ValueError):
         x = Alternative_Data.Data('FaultyData_UnitTest.csv')
         x.value_range()
+
+def test_data_is_good():
+    good_test_data = Alternative_Data.Data('GoodData_UnitTest.csv')
+    good_test_data.column_check()
+    good_test_data.value_type()
+    good_test_data.value_range()
+    assert good_test_data.column_check_result is True
+    assert good_test_data.value_type_result is True
+    assert good_test_data.value_range_result is True
