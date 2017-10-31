Fixed minor pep-8 issues. Also, returned to previous set-up where an error is raised if the user inputs an averaging time greater than the ECG acquisition time.

Vitals.py
@@ -123,11 +123,9 @@ class Vitals:
         # use a moving average to calculate avg_hr from inst_hr
 
         self.avg_hr_array = np.convolve(self.inst_hr_array,
-                                      np.ones(final_ind) / final_ind, 'same')
+                                        np.ones(final_ind) / final_ind, 'same')
         self.avg_hr_array = np.around(self.avg_hr_array)
         self.avg_hr_array = self.avg_hr_array.astype(int)
-
-
 
         # # calculate average hr array
         # time_array_sliced = self.peak_time_array[:final_ind+1]