4f79c9e69f18919c9e85cf1f73c3cd4975af11be
Jawad Hoballah <jawad.hoballah@duke.edu>
2017-10-30 20:16:03 -0400

    Updated avg_hr to calculate an array of avg_hr values from the inst_hr array.

--- Vitals.py
+++ Vitals.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+import numpy as np
 
 
 class Vitals:
@@ -15,7 +16,7 @@ class Vitals:
         :param peak_time_array: holds time values at an identified peak
         :param time_array: original time array that was passed in
         """
-        self.avg_hr_val = None
+        self.avg_hr_array = []
         self.inst_hr_array = []
         self.averaging_time = averaging_time
         self.peak_time_array = peak_time_array
@@ -62,23 +63,7 @@ class Vitals:
         if averaging_time_sec > max_acq_time:
             print("Your averaging time is longer than the ECG acquisition time"
                   ", try a new value")
-            averaging_time_sec = max_acq_time
             raise ValueError
-        # find the smallest distance between the averaging_time
-        final_ind = 0
-        final_min = abs(self.peak_time_array[0] - averaging_time_sec)
-
-        # for i in range(1, len(self.time_array)):
-        #     if i == 1:
-        #         dt_first_beat = self.time_array[i-1]
-        #         self.inst_hr_array.insert(i-1, self.MIN_TO_SEC
-        #                                   * (1/dt_first_beat))
-        #
-        #     dt_first_beat = self.time_array[i] - self.time_array[i-1]
-        #     self.inst_hr_array.insert(i-1, self.MIN_TO_SEC * 1/dt_first_beat)
-        #
-        #     # self.inst_hr_array.insert(i, self.MIN_TO_SEC * 1 / dt_first_beat)
-        #     self.inst_hr_array[i-1] = int(round(self.inst_hr_array[i-1]))
 
         # calculate instantaneous hr array
         for i in range(0, len(self.peak_time_array) - 1):
@@ -90,32 +75,61 @@ class Vitals:
 
         # initialize variables for interpolated inst_hr_array
         temp_inst_hr_array = []
-        lower_bound = 0
-        upper_bound = self.peak_time_array[0]
+        lower_bound = self.peak_time_array[0]
+        upper_bound = self.peak_time_array[1]
         counter = 0
 
+        # interpolate instantaneous hr values for all peak times
         for time in self.time_array:
+            # append first inst_hr for times before first peak
             if time < self.peak_time_array[0]:
                 temp_inst_hr_array.append(self.inst_hr_array[0])
-            elif lower_bound <= time < upper_bound:
+
+            # append the inst_hr value from the inter-peak time interval
+            elif lower_bound <= time <= upper_bound:
                 temp_inst_hr_array.append(self.inst_hr_array[counter])
+
+            # append next inst_hr value after passing the inter-peak interval
             elif time > upper_bound:
                 lower_bound = upper_bound
                 counter = counter + 1
                 upper_bound = temp_inst_hr_array[counter]
                 temp_inst_hr_array.append(self.inst_hr_array[counter])
+            # append last inter-peak inst_hr value for times after the last peak
             elif time > max(self.peak_time_array):
-                temp_inst_hr_array.append\
-                    (self.inst_hr_array[max(self.peak_time_array)])
+                temp_inst_hr_array.append(self.inst_hr_array[
+                                              max(self.peak_time_array)])
+        # set inst_hr_array to the interpolated inst_hr_array
         self.inst_hr_array = temp_inst_hr_array
-        # calculate average hr array
-        for i in range(0, len(self.peak_time_array)):
 
-            min_val = abs(self.peak_time_array[i] - averaging_time_sec)
+        # find the smallest distance between the averaging_time
+        final_ind = 0
+        final_min = abs(self.time_array[0] - averaging_time_sec)
+        # final_min = abs(self.time_array[0] - averaging_time_sec)
+
+        for i in range(0, len(self.time_array)):
+
+            min_val = abs(self.time_array[i] - averaging_time_sec)
             if min_val < final_min:
                 final_min = min_val
                 final_ind = i
+        # for i in range(0, len(self.peak_time_array)):
+        #
+        #     min_val = abs(self.peak_time_array[i] - averaging_time_sec)
+        #     if min_val < final_min:
+        #         final_min = min_val
+        #         final_ind = i
 
-        time_array_sliced = self.peak_time_array[:final_ind+1]
-        self.avg_hr_val = \
-            int(round((len(time_array_sliced))/self.averaging_time))
+        # use a moving average to calculate avg_hr from inst_hr
+
+        self.avg_hr_array = np.convolve(self.inst_hr_array,
+                                      np.ones(final_ind) / final_ind, 'same')
+        self.avg_hr_array = np.around(self.avg_hr_array)
+        self.avg_hr_array = self.avg_hr_array.astype(int)
+
+
+
+        # # calculate average hr array
+        # time_array_sliced = self.peak_time_array[:final_ind+1]
+        # self.avg_hr_val = \
+        #     int(round((len(time_array_sliced))/self.averaging_time))
--- test_Vitals.py
+++ test_Vitals.py
@@ -1,7 +1,6 @@
 import pytest
 import Vitals
 import Processing
-import Data
 import numpy as np
 
 """
@@ -10,14 +9,8 @@ Unit tests for Vitals class
 
 """
 
-test_data = Data.Data('test_data5.csv')
-test_data.extraction()
-full_time_array = test_data.hr_data[:,0]
-processed_data = Processing.Processing()
-processed_data.ecg_peakdetect(test_data.hr_data)
-array_test_time = processed_data.t
-
 t = np.arange(0, 60, 0.001)
+t_midpoint = int(t.size / 2)
 signal = abs(np.sin(t*np.pi)**3)
 
 # combine t and signal arrays
@@ -27,96 +20,110 @@ sine_data.ecg_peakdetect(sine_array_test)
 sine_array_test_time = sine_data.t
 
 
-def test_nonzero_avgingtime():
+def test_nonzero_averaging_time():
     """
-    .. function:: test_nonzero_avgingtime():
+    .. function:: test_nonzero_averaging_time():
     Test if the averaging time is a non-zero and positive number,
     otherwise throw error
     """
 
     with pytest.raises(ValueError):
-        Vitals.Vitals(0, array_test_time, full_time_array)
+        Vitals.Vitals(0, sine_array_test_time, t)
 
 
-def test_validlen_avgingtime():
+def test_valid_len_averaging_time():
     """
-    .. function:: test_validlen_avgingtime():
+    .. function:: test_valid_len_averaging_time():
     Test if the averaging time less than or equal to the length of ECG
     acquisition time, otherwise throw error
     """
 
     with pytest.raises(ValueError):
-        Vitals.Vitals(15, array_test_time, full_time_array)
+        Vitals.Vitals(15, sine_array_test_time, t)
 
 
-def test_isnumber_avgingtime():
+def test_is_number_averaging_time():
     """
-    .. function:: test_isnumber_avgingtime():
+    .. function:: test_is_number_averaging_time():
     Test if the averaging time is a number, otherwise throw error
     """
 
     with pytest.raises(ValueError):
-        Vitals.Vitals("word", array_test_time, full_time_array)
+        Vitals.Vitals("word", sine_array_test_time, t)
 
 
-def test_fraction_divby0():
+def test_fraction_div_by_0():
     """
-    .. function:: test_fraction_divby0():
+    .. function:: test_fraction_div_by_0():
     Test if the averaging time is a valid fraction, throw error if zero
     division occurs
     """
 
     with pytest.raises(ZeroDivisionError):
-        Vitals.Vitals('1/0', array_test_time, full_time_array)
+        Vitals.Vitals('1/0', sine_array_test_time, t)
 
 
-def test_fraction_validsyntax():
+def test_fraction_valid_syntax():
     """
-    .. function:: test_fraction_validsyntax():
+    .. function:: test_fraction_valid_syntax():
     Test if the averaging time uses valid fraction syntax
     """
 
     with pytest.raises(ValueError):
-        Vitals.Vitals('1/2/3', array_test_time, full_time_array)
+        Vitals.Vitals('1/2/3', sine_array_test_time, t)
 
 
-def test_avghr_withfraction():
+def test_avg_hr_with_fraction():
     """
-    .. function:: test_avghr_withfraction():
+    .. function:: test_avg_hr_with_fraction():
     Test if the avghr function calculates the correct avg HR using a fraction
     """
-    x = Vitals.Vitals('5/60', array_test_time, full_time_array)
-    assert x.avg_hr_val == 84
+    x = Vitals.Vitals('5/60', sine_array_test_time, t)
+    assert x.avg_hr_array[t_midpoint] == 60
 
 
-def test_avghr_with_float_as_string():
+def test_avg_hr_with_float_as_string():
     """
-    .. function:: test_avghr_with_float_as_string():
+    .. function:: test_avg_hr_with_float_as_string():
     Test if the avghr function calculates the correct avg HR using a decimal
     value passed as a string
     """
-    x = Vitals.Vitals('.083333333', array_test_time, full_time_array)
-    assert x.avg_hr_val == 84
+    x = Vitals.Vitals('.33333333', sine_array_test_time, t)
+    assert x.avg_hr_array[t_midpoint] == 60
 
 
-def test_avghr_with_float():
+def test_avg_hr_with_float():
     """
-    .. function:: test_avghr_with_float():
+    .. function:: test_avg_hr_with_float():
     Test if the avghr function calculates the correct avg HR using a
     decimal value
     """
 
-    x = Vitals.Vitals(0.083333333, array_test_time, full_time_array)
+    x = Vitals.Vitals(0.33333333, sine_array_test_time, t)
+    assert x.avg_hr_array[t_midpoint] == 60
 
-    assert x.avg_hr_val == 84
+
+def test_avg_hr_length():
+
+    x = Vitals.Vitals(0.33333333, sine_array_test_time, t)
+    assert len(x.avg_hr_array) == len(t)
 
 
-def test_insthr():
+def test_inst_hr_val():
     """
-        .. function:: test_instHR():
+        .. function:: test_inst_hr_val():
         Tests if instant heart rate calculated is equal to T * 60s/min
     """
     x = Vitals.Vitals('5/60', sine_array_test_time, t)
 
     for i in range(0, len(x.inst_hr_array)):
         assert x.inst_hr_array[i] == 60
+
+
+def test_inst_hr_length():
+    """
+        .. function:: test_inst_hr_length():
+        Tests if instant heart rate array is correct length
+    """
+    x = Vitals.Vitals('5/60', sine_array_test_time, t)
+    assert len(x.inst_hr_array) == len(t)
